2bd9d0f7dcbabd34b3ee7e9a60eec68967fd252e
Raph <rj42guez@gmail.com>
2024-09-26 16:39:33 +0800

    chagnes

---
+++ website/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/programs')
+def programs():
+    return render_template("programs.html")
+
+@app.route('/members')
+def members():
+    return render_template("members.html")
+
+@app.route('/public-policy-presentations')
+def publicpolicypresentations():
+    return render_template("public-policy-presentations.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
