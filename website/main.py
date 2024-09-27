b8ec082b9450243be953565543230264e3cc4fa8
Raph <rj42guez@gmail.com>
2024-09-27 21:46:10 +0800

    hrllo

--- website/main.py
+++ website/main.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
+
 @app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/home')
 def home():
     return render_template("home.html")
 
