a45cdf03784229bb79484c0f13069fe6b24e60ea
Raph <rj42guez@gmail.com>
2024-09-28 00:25:46 +0800

    help

--- website/main.py
+++ website/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route('/about')
 def about():
