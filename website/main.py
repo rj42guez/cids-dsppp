40a0cd3f4158ebb8a2cccf4c002a843825a936e3
Raph <rj42guez@gmail.com>
2024-09-27 21:47:08 +0800

    hrllo

--- website/main.py
+++ website/main.py
@@ -7,10 +7,6 @@ app = Flask(__name__, template_folder='templates')
 def home():
     return render_template("home.html")
 
-@app.route('/home')
-def home():
-    return render_template("home.html")
-
 @app.route('/about')
 def about():
     return render_template("about.html")
