a4af0ad79fd1d0dbfa0a748081b4d93d592fbb9c
Raph <rj42guez@gmail.com>
2024-10-01 12:15:06 +0800

    members

---
+++ website/static/index.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById('navbarSupportedContent');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { // Adjust this value based on when you want the color to change
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
