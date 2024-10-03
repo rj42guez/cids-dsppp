385bbc8d59ddc724a3ade06e5c16ce9a68b42f50
Raph <rj42guez@gmail.com>
2024-10-03 13:41:43 +0800

    lmao

--- website/static/index.js
+++ website/static/index.js
@@ -1,9 +1,27 @@
 const navbar = document.getElementById('navbarMain');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) { // Adjust this value based on when you want the color to change
+    if (window.scrollY > 20) { // Adjust this value based on when you want the color to change
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
     }
 });
+
+function mobileNavbar() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  } 
