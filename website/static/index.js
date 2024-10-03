f6f599464afcd448b50f6047edf5d60f3852ce06
Raph <rj42guez@gmail.com>
2024-10-03 14:09:43 +0800

    lmao

--- website/static/index.js
+++ website/static/index.js
@@ -16,12 +16,10 @@ function mobileNavbar() {
       x.style.display = "block";
     }
   } 
-
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+  function openNav() {
+    document.getElementById("mobileNavbar").style.width = "100%";
   }
   
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  } 
+  function closeNav() {
+    document.getElementById("mobileNavbar").style.width = "0%";
+  }
